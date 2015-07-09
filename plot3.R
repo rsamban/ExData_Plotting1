df5eb05eeca676d8c8d4b9a348ce82e0ebdf4fd9
U-Ramesh-PC\Ramesh <Ramesh@Ramesh-PC.(none)>
2015-07-09 02:39:46 -0600

    Coursera Exploratory Data Analysis - course project 1

---
+++ plot3.R
@@ -0,0 +1,22 @@
+#create tmp file
+temp <- tempfile()
+#download data to temp file
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",temp)
+#unzip and load data
+allData <- read.csv(unz(temp,"household_power_consumption.txt"), sep=";")
+#delete temp file
+unlink(temp)
+#filter and massage data
+Feb2DayData <- allData[allData$Date %in% c('1/2/2007','2/2/2007'),]
+Feb2DayData$dateTime <- paste(Feb2DayData$Date,Feb2DayData$Time,sep=" ")
+Feb2DayData$dateTime <- strptime(Feb2DayData$dateTime, '%d/%m/%Y %H:%M:%S')
+Feb2DayData$Global_active_power <- as.numeric(Feb2DayData$Global_active_power)
+#open png graphics device and create plot
+png("plot3.png", width=480,height=480)
+plot(Feb2DayData$dateTime, Feb2DayData$Sub_metering_1, type="n", xlab="", ylab="Energy sub metering")
+lines(Feb2DayData$dateTime, Feb2DayData$Sub_metering_1, col="black")
+lines(Feb2DayData$dateTime, Feb2DayData$Sub_metering_2, col="red")
+lines(Feb2DayData$dateTime, Feb2DayData$Sub_metering_3, col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black","red","blue"), lty=1)
+#close the png device
+dev.off()
